b7af364c42b3e5bc756b6f9f82d81e8cadc57d32
MoroniAg <moroniaguilera1993@gmail.com>
2026-09-12 16:35:50 -0500

    Add tent data JSON and update Vite configuration for product pages
    - Created a new JSON file `tents.json` containing detailed information about various tents, including their specifications, images, and pricing.
    - Updated `vite.config.js` to simplify product page routing by consolidating multiple product HTML files into a single `index.html` for easier management.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,6 +22,16 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterend", clear = false) {
+  const htmlStrings = list.map(templateFn);
+
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
